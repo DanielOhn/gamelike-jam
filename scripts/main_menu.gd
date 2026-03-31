8cc47a07a1305bf8095ac73fe981eda157c09698
Daniel Ohn <ohndaniel@gmail.com>
2026-03-31 07:01:10 -0700

    Add video as ogv file.  Add video to main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,19 @@
 extends Node3D
 
 @onready var audio_stream_player = $AudioStreamPlayer
+@onready var video_stream_player = $Control/VideoStreamPlayer
 
 func _on_play_button_pressed():
+	video_stream_player.play()
+	audio_stream_player.stop()
+	await video_stream_player.finished
+	
 	get_tree().change_scene_to_file("res://scenes/Level.tscn")
 
 func _physics_process(delta):
 	if !audio_stream_player.playing:
 		audio_stream_player.play()
+		
+	if video_stream_player.is_playing():
+		if Input.is_action_pressed("ui_cancel"):
+			get_tree().change_scene_to_file("res://scenes/Level.tscn")
