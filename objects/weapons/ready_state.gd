eb69dc5b4123e5ce8e11959b3a4fef9b01ec6453
Daniel Ohn <ohndaniel@gmail.com>
2026-04-17 18:40:26 -0700

    Actual wand ammo bug fix

--- objects/weapons/ready_state.gd
+++ objects/weapons/ready_state.gd
@@ -9,15 +9,18 @@ func enter() -> void:
 	state_machine.anim_player.play(anim_text)
 
 func physics_update(_delta: float) -> void:
+	
 	if reload_state and state_machine.ammo_capacity > 0:
 		if state_machine.current_ammo <= 0 and state_machine.total_ammo > 0:
 			state_machine.switch_state(reload_state)
 			
 		if Input.is_action_just_pressed("reload") and state_machine.total_ammo > 0:
 			state_machine.switch_state(reload_state)
-	else:
+	elif state_machine.weapon_slot.current_weapon.name == "Wand":
 		if state_machine.current_ammo <= 0:
 			state_machine.switch_state(reload_state)
 			
 		if Input.is_action_just_pressed("reload"):
 			state_machine.switch_state(reload_state)
+	#else:
+		
--- objects/weapons/reload_state.gd
+++ objects/weapons/reload_state.gd
@@ -5,9 +5,11 @@ class_name ReloadState
 @export var reload_anim: String
 
 func refill_ammo():
-	state_machine.total_ammo -= state_machine.max_clip - state_machine.current_ammo
+	if state_machine.weapon_slot.current_weapon.name != "Wand":
+		state_machine.total_ammo -= state_machine.max_clip - state_machine.current_ammo
+		state_machine.total_ammo = clamp(state_machine.total_ammo, 0, state_machine.ammo_capacity)
 	state_machine.current_ammo = state_machine.max_clip
-	state_machine.total_ammo = clamp(state_machine.total_ammo, 0, state_machine.ammo_capacity)
+	
 	
 
 func swap_state():
